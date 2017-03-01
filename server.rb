d8bdf6c76a310536e04bef5e2678ef032d2aaaf5
Max <enrique.guido@yahoo.com>
2017-03-01 17:08:47 -0500

    Commiting server.rb file

--- server.rb
+++ server.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
-get '/' do
-	"Hello world"
-end
+# get '/' do
+#   current_time = Time.now
+#   weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+# 	"Hello! Today is #{weekdays[current_time.wday]}"
+# end
 
 get "/sinatra" do
 	"Hello Sinatra"
+end
+
+get '/' do
+  send_file "public/hello.txt"
 end
